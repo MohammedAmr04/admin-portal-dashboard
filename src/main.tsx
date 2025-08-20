e186440ff736dca3bdb7f2dce3285e06c5214130
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-08-20 22:29:29 +0300

    Initial project setup with Vite, React, and TypeScript. Added configuration files for ESLint, Prettier, and TypeScript. Included basic application structure with routing, theming, and context providers. Added global styles and a sample SVG icon.

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/global.css'
+import '@ant-design/v5-patch-for-react-19'
+
+import App from './App.tsx'
+import { ReactQueryProvider } from './services/context/ReactQueryProvider.tsx'
+import UiProvider from './services/context/UiProvider.tsx'
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ReactQueryProvider>
+      <UiProvider>
+        <App />
+      </UiProvider>
+    </ReactQueryProvider>
+  </StrictMode>
+)
---
+++ src/services/constants/ANT_CONFIG.ts
@@ -0,0 +1,47 @@
+import { type ThemeConfig, theme } from 'antd'
+import { MAIN_COLORS } from './COLORS'
+
+const MAIN_THEME_TOKEN: ThemeConfig['token'] = {
+  borderRadius: 8,
+  borderRadiusLG: 8,
+  borderRadiusOuter: 8,
+  // fontFamily: var(--antd-font),
+  fontSize: 14,
+  fontSizeXL: 16,
+  controlHeight: 34,
+  controlHeightLG: 51,
+}
+
+export const ANTD_THEME: ThemeConfig = {
+  algorithm: theme.defaultAlgorithm,
+  token: {
+    colorPrimary: MAIN_COLORS.light.primary,
+    colorBgBase: MAIN_COLORS.light.background,
+    colorTextBase: MAIN_COLORS.light.text + 'b6',
+    colorBorder: MAIN_COLORS.light.border,
+    colorBgElevated: MAIN_COLORS.light.elevated,
+    ...MAIN_THEME_TOKEN,
+  },
+  components: {
+    Segmented: {
+      trackBg: MAIN_COLORS.light.card,
+    },
+  },
+}
+export const ANTD_THEME_DARK: ThemeConfig = {
+  algorithm: theme.darkAlgorithm,
+  token: {
+    colorPrimary: MAIN_COLORS.dark.primary,
+    colorBgBase: MAIN_COLORS.dark.background,
+    colorTextBase: MAIN_COLORS.dark.text + 'b6',
+    colorBorder: MAIN_COLORS.dark.border,
+    colorBgElevated: MAIN_COLORS.dark.elevated,
+
+    ...MAIN_THEME_TOKEN,
+  },
+  components: {
+    Segmented: {
+      trackBg: MAIN_COLORS.dark.card,
+    },
+  },
+}
