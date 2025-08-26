35c217a11b9d7185a3a7e5a6ec096189c7403cc8
MohammedAmr04 <mohammedamr200497@gmail.com>
2025-08-26 21:27:42 +0300

    Add ConfirmationModal and SuccessModal components
    - Introduced ConfirmationModal for user confirmation actions with customizable title, icon, and handlers.
    - Added SuccessModal to display success messages with a title and icon.
    - Both modals utilize Ant Design's Modal component for consistent styling and behavior.
    - Enhanced global CSS for improved modal appearance and layout.

---
+++ src/components/ui/models/ConfirmationModal.tsx
@@ -0,0 +1,54 @@
+import React, { type ReactNode } from 'react'
+import { Modal, Button } from 'antd'
+
+interface ConfirmationModalProps {
+  visible: boolean
+  title: string
+  icon: ReactNode
+  onCancel: () => void
+  onConfirm: () => void
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+  visible,
+  onCancel,
+  title,
+  icon,
+  onConfirm,
+}) => {
+  return (
+    <Modal
+      open={visible}
+      onCancel={onCancel}
+      styles={{ footer: { textAlign: 'center' } }}
+      classNames={{
+        content: '!bg-background-dark rounded-lg',
+      }}
+      footer={[
+        <Button onClick={onCancel} className="!bg-text w-29  !text-[#1C122E]">
+          Cancel
+        </Button>,
+        <Button
+          key="confirm"
+          className="w-29"
+          onClick={onConfirm}
+          type="primary"
+        >
+          Yes
+        </Button>,
+      ]}
+      centered
+      closable={false}
+      width={450}
+    >
+      <div className="flex flex-col items-center justify-center p-6">
+        <div className="bg-success/10 rounded-xl p-3 w-15 h-15 flex items-center justify-center mb-4">
+          {icon}
+        </div>
+        <p className="text-text text-center">{title} </p>
+      </div>
+    </Modal>
+  )
+}
+
+export default ConfirmationModal
