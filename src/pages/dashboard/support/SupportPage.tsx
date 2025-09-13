91056e2046b0fac3bd5f305cbf32c7d9dd290385
Abdelrahman <AbdelrahmanHelmySalama@outlook.com>
2025-09-13 19:21:09 +0300

    (feat)SupportExport + (feat)SupportResponsive

--- src/pages/dashboard/support/SupportPage.tsx
+++ src/pages/dashboard/support/SupportPage.tsx
@@ -2,11 +2,15 @@ import TicketDrawer from '@/components/support/drawers/TicketDrawer'
 import HeaderSupport from '@/components/support/header/HeaderSupport'
 import { useState } from 'react'
 import { ContentSupport } from '@/components/support/contentSupport/ContentSupport'
+import type { ITicket } from '@/services/types/ticket'
 
 const SupportPage = () => {
   const [drawer, setDrawer] = useState(false)
   const [selectedTicket, setSelectedTicket] = useState<number | undefined>(
     undefined
+  )
+  const [selectedExportTickets, setSelectedExportTickets] = useState<ITicket[]>(
+    []
   )
 
   const handleOpenTicket = (ticketID: number) => {
@@ -19,10 +23,17 @@ const SupportPage = () => {
     setDrawer(false)
   }
 
+  const handleExportTickets = (tickets: ITicket[]) => {
+    setSelectedExportTickets(tickets)
+  }
+
   return (
     <>
-      <HeaderSupport />
-      <ContentSupport onOpenTicket={handleOpenTicket} />
+      <HeaderSupport selectedExportTickets={selectedExportTickets} />
+      <ContentSupport
+        onOpenTicket={handleOpenTicket}
+        onSelectExportTickets={handleExportTickets}
+      />
       {drawer && selectedTicket && (
         <TicketDrawer ticketID={selectedTicket} onClose={handleCloseTicket} />
       )}
